c5bed419b85cf781b7806f0b57bd5ca3e0ee46a7
Chris Harding <charding@iastate.edu>
2020-05-21 13:21:53 -0500

    Converted to Python 3

--- src/Radiation_maps_from_ECMWF40_data_yearly.py
+++ src/Radiation_maps_from_ECMWF40_data_yearly.py
@@ -4,6 +4,8 @@
 # given a netCDF file of 1 or more variables, calculate the mean and
 # plot it with basemap (matplotlib) and save it as a geotif file
 
+# Ch: Apr. 2020: converted to Python 3 
+
 import netCDF4
 import os, sys
 import datetime
@@ -18,7 +20,8 @@ from mpl_toolkits.basemap import Basemap, addcyclic
 
 # helper function: get date based on number of hours since Jan.1, 1900 (the 0 for this netcdf file)
 def get_datetime(nhours): 
-    delta = datetime.timedelta(hours=nhours)  
+    w = nhours / (24 * 7) 
+    delta = datetime.timedelta(weeks=w)  
     date = datetime.datetime(1900,  1, 1) + delta # see ncheader.txt
     return date 
 #
@@ -34,18 +37,14 @@ def save_as_geotiff(data, filename, geotransform, projection):
 		      top left y, rotation - 0 if image is "north up", n-s pixel resolution
     projection - GDAL projection: "" for unprojected 
                  Note: this is IGNORED for now! GDAL supports projections such as NAD83 or UTM, but I've not tested 
-		 anything but unprojected with WGS84
-		       
-    		 
+		               anything but unprojected with WGS84
     """
-
     # check if we have a float array
     t = str(data.dtype)
     if not "float" in t: 
-	print "save_as_geotiff(): data array must be float!"
-	return
+        print("save_as_geotiff(): data array must be float!")
+        return
 
-    
     driver  = gdal.GetDriverByName('GTiff')
     nrows,ncols = data.shape
     num_bands = 1
@@ -61,17 +60,18 @@ def save_as_geotiff(data, filename, geotransform, projection):
     # Write data into file
     output_raster.GetRasterBand(1).WriteArray(data)
     output_raster.GetRasterBand(1).ComputeStatistics(False) # False -> write full stats, not just the overview  stats
-    output_raster = None # close 
+    output_raster = None # close buffer
     
-    print "Created geotiff", filename
+    print("Created geotiff", filename)
     
 
 # ---------------------- MAIN -----------------------------
 start_folder = os.getcwd()
-print "in folder", start_folder
+print("in folder", start_folder)
+
 
 # netcdf file, dl from http://apps.ecmwf.int/datasets/data/era40-moda
-# Monthly means of Daily means, moda, 6, 2.5°, 1957-09-01...2002-08-01, Forecast, 40 years reanalysis
+# Monthly means of Daily means, moda, 6, 2.5, 1957-09-01...2002-08-01, Forecast, 40 years reanalysis
 # This file contains:  Surface net solar radiation, Surface net thermal radiation, Top net solar radiation, 
 #  Top net thermal radiation  (SSR, STR, TSR, TTR)
 
@@ -99,18 +99,19 @@ print "in folder", start_folder
 		#tsr:units = "J m**-2" ;                <- to get the actual J/m2 value
 		#tsr:long_name = "Top net solar radiation" ;
 		#tsr:standard_name = "toa_net_upward_shortwave_flux" ;
-		
+
+
 # name of netcdf file on disk
 fn = "ECMWF40_moda_Sep1957_Aug2002_SSR_STR_TSR_TTR.nc"  
-		
+
 ncattr = {} # dict for storing attributes
 
 # variables to plot
-#varnames = [ "ttr", "tsr", "str", "ssr"] 
-varnames = [ "ttr"] 
+#varnames = ["ttr", "tsr", "str", "ssr"] 
+varnames = ["ttr"]
 
-print "reading netcdf file", fn
-nc = netCDF4.Dataset(fn,diskless=False) 
+print("reading netcdf file", fn)
+nc = netCDF4.Dataset(fn, diskless=False) 
 
 # from the nc file, grab some attribute values we will need later (as a dict)
 # the time array, the lat/lon arrays and the value array (as numpy arrays)
@@ -118,48 +119,53 @@ for varname in varnames:
     v  = nc.variables 
     curr_var = v[varname] # the nc variable
     
-    print "working on", varname
-    for i in zip(curr_var.dimensions, curr_var.shape): # print out number of cells for each dimentsion
-        print "\t",i[0], i[1]
+    print("working on", varname)
+    for i in zip(curr_var.dimensions, curr_var.shape): # print out number of cells for each dimension
+        print("\t",i[0], i[1])
         
-    # get attributes of current var as a dict (see ncheader), see http://home.strw.leidenuniv.nl/~sfinx/netcdf4.html
+    # get attributes of current var as a dict (see ncheader), 
+    # see http://home.strw.leidenuniv.nl/~sfinx/netcdf4.html
     for a in ("scale_factor", "add_offset", "missing_value", "units", "long_name"):
-        ncattr[a] = curr_var.getncattr(a)
-        print "\t", a, ncattr[a]
+       try:
+           ncattr[a] = curr_var.getncattr(a)
+           print("\t", a, ncattr[a])
+       except:
+           print(a, "not found")
     
     # get the time array (1D), each with the time in secs from 1/1/1900 for each of the 540 time slices
     time_ = np.array(v["time"]) 
     total_hours = time_[-1] - time_[0]
-    print "\ttime: from", get_datetime(time_[0]), "to", get_datetime(time_[-1]), "total hours:",total_hours
-    print
-
-    # get lat/lon arrays (1D)
-    lat = np.array(nc.variables["latitude"])
-    #print lat, lat.shape, len(lat)
-    lon = np.array(nc.variables["longitude"])    
+    print("\ttime: from", get_datetime(time_[0]), "to", get_datetime(time_[-1]), "total hours:",total_hours)
+    print()
 
     # get data value array (3D)
     vl = np.array(v[varname])
-    print "data values", vl.shape, vl.dtype    # we will later scale  and offset them  
+    print("data values", vl.shape, vl.dtype)  # we will later scale  and offset them  
+    print("global min %f  mean %f  max %f" % (vl.min(), vl.mean(), vl.max()))
     
-    """
-    # Sanity check: sample a spread of 10 values (vals) via histogram, scale and offset them to
-    # first get J/m2, then convert to W/m2 by dividing by the number of seconds per month.
-    bins,vals = np.histogram(vl) # 10 bin histogram with bin values in vals
-    sc  = ncattr["scale_factor"]
-    ofs = ncattr["add_offset"]
-    time_in_secs = 730.46 * 3600  # seconds per timestep using an average number of hours per month 
-    print "\nSanity check with histrogram-samples values:"
-    print "raw value, *scale+offset (J/m2), /time (W/m2)"
-    for v in vals:
-        J_val = v * sc + ofs                       # http://home.strw.leidenuniv.nl/~sfinx/netcdf4.html: If both scale_factor and add_offset attributes are present, the data are first scaled before the offset is added.
-        print int(v), J_val, J_val / time_in_secs  # raw, convert to J, convert J to W
-    print
-    """
+    # get lat/lon arrays (1D)
+    lat = np.array(nc.variables["latitude"])
+    #print lat, lat.shape, len(lat)
+    lon = np.array(nc.variables["longitude"])        
+    
+    if 0:  # <- set to 1 to run the sanity check
+        # Sanity check: sample a spread of 10 values (vals) via histogram, scale and offset them to
+        # first get J/m2, then convert to W/m2 by dividing by the number of seconds per month.
+        bins,vals = np.histogram(vl) # 10 bin histogram with bin values in vals
+        sc  = ncattr["scale_factor"]
+        ofs = ncattr["add_offset"]
+        time_in_secs = 730.46 * 3600  # seconds per timestep using an average number of hours per month 
+        print "\nSanity check with histogram-samples values:"
+        print "raw value, *scale+offset (J/m2), /time (W/m2)"
+        for v in vals:
+            J_val = v * sc + ofs                       # http://home.strw.leidenuniv.nl/~sfinx/netcdf4.html: If both scale_factor and add_offset attributes are present, the data are first scaled before the offset is added.
+            print(int(v), J_val, J_val / time_in_secs)  # raw, convert to J, convert J to W
+        print()
+    
     # For the sanity check we just approximated the number of seconds per month but to properly convert the data to W/m2
     # we really need to know the exact number of secs for each timeslice. 
     # _time contains all month as timestamps in hours(!)
-    # We get the duration of each month in hours by subtracting athe timestamp value of the next month 
+    # We get the duration of each month in hours by subtracting the timestamp value of the next month 
     # from the timestamp value of the current month (works for all except the very last month ...),
     # and multiplying this difference by 3600 to get the duration in seconds.
     
@@ -171,28 +177,29 @@ for varname in varnames:
            #print i,d
            i += 1    
     duration[i] = duration[i-12] # last month: use same as a year ago  
-    #print i,duration[i]  
+    #print(i,duration[i])  
        
     
     # convert each slice from J/m**2 to Watts/m**2 by div. it by its duration (in secs)
     sc  = ncattr["scale_factor"]
     ofs = ncattr["add_offset"]    
     for i,curr_slice in enumerate(vl):  # go through all slices, for loop will iterate by time dimension (axis=0 [time, lat, lon])
-        #print curr_slice.shape # check that we have a 2D slice 
+        #print(curr_slice.shape) # check that we have a 2D slice 
         curr_slice = curr_slice * sc + ofs #... data are first scaled before the offset is added.  http://home.strw.leidenuniv.nl/~sfinx/netcdf4.html:
         curr_slice /= duration[i] # convert J to W, duration is number of secs per month 
-        #print i, duration[i], np.mean(curr_slice)
+        #print(i, duration[i], np.mean(curr_slice))
         vl[i] = curr_slice # overwrite with Watt/s array
    
     # For ttr, negate values (as per Diana's request so the 106 students don't have to deal with negative energy)
     if varname == "ttr" or varname == "str": vl = -vl
      
     # get the mean along then time axis
-    print "global min %f  mean %f  max %f" % (vl.min(), vl.mean(), vl.max()) # mean of converted 3D array
+    print("global min %f  mean %f  max %f" % (vl.min(), vl.mean(), vl.max())) # mean of converted 3D array
     mean = vl.mean(axis=0)
-    #print "mean"; b,v = np.histogram(mean);print b;print v
+    #print("mean"); b,v = np.histogram(mean);print(b);print(v)
     #std = vl.std(axis=0)
-    #print "std"; b,v = np.histogram(std);print b;print v
+    #print("std"); b,v = np.histogram(std);print(b);print(v)
+
 
     #
     # Export as geotiff using GDAL
@@ -204,7 +211,7 @@ for varname in varnames:
     yres = (180)/float(vl.shape[1]-1) # lat   -1 as we have 73 not  72
     xmin = 0; ymin = 90
     
-    # I assume the data is cell centered I so offset by 1/2 a cell
+    # I assume the data is cell centered I so offset by 1/2 of a cell
     xmin -= xres/2.0 
     ymin += yres/2.0 
     #print "geotransform:", xmin, xres, ymin, yres   
@@ -243,14 +250,14 @@ for varname in varnames:
     x, y = np.meshgrid(lon, lat)  # makes 2D arrays from 1D lat and 1D lon
 
     # plot at median      
-    medians = range(-90,181,90) # must be -180 to +180
-    #medians = [0]
+    #medians = range(-90,181,90) # must be -180 to +180
+    medians = [90]
     for med in medians:        
         
         # colormap(s) used for pcolormesh 
         # https://jakevdp.github.io/blog/2014/10/16/how-bad-is-your-colormap/
         #cmaps  = [plt.cm.nipy_spectral, plt.cm.CMRmap]
-	cmaps  = [plt.cm.CMRmap]
+        cmaps  = [plt.cm.CMRmap]
         """
                   plt.cm.gist_ncar,
                   plt.cm.gist_rainbow,
@@ -262,23 +269,23 @@ for varname in varnames:
                   plt.cm.YlGnBu_r,
         """
         for cmap in cmaps:
-            print "colormap", cmap.name        
+            print("colormap", cmap.name)        
         
 	    # draw a basemap and keep handle 
             m = Basemap(
                 #llcrnrlon, llcrnrlat, ucrnrlon, urcrnrlat, 
                 resolution='c', # c,l,i,h,f 
 	        #area_thresh = 1000,
-                projection='robin', # robinson projection
+                #projection='robin', # robinson projection
                 #projection='moll',
                 #projection='kav7',
                 #projection='cyl',
                 #projection='hammer',
-                lon_0=med,    # sets the median for some projections         
+                #lon_0=med,    # sets the median for some projections         
                 ) 
 
             m.drawcoastlines(linewidth=1.5, color='Black')
-	    m.drawcountries(linewidth=0.01, color='#0B0B0B')
+            m.drawcountries(linewidth=0.01, color='#0B0B0B')
             #m.drawrivers(linewidth=1, color='Blue')
 	    #m.drawstates(linewidth=1.5) # US states
  	    
@@ -292,7 +299,7 @@ for varname in varnames:
     
 
             # draw data (2-D array) as colored cells
-            pcm = m.pcolormesh(x,y,
+            pcm = m.pcolormesh(lon,lat,
                                mean, 
                                latlon=True, 
                                cmap=cmap, 
@@ -302,6 +309,7 @@ for varname in varnames:
             cb_ticks = range(-500,500,20) # tick marks on color bar
             cbar = m.colorbar(pcm,location='bottom',pad="10%", ticks=cb_ticks)
             cbar.set_label('W / m**2')    
+            
             
             # contour plot
             intv = 40 # stepsize for intervals
@@ -327,7 +335,7 @@ for varname in varnames:
             plt.title(title_str, fontsize=18)    
             
             # put plot on a specific page size
-            fig = mpl.pyplot.gcf() # get's current figure
+            fig = mpl.pyplot.gcf() # get current figure
             fig.set_size_inches(17, 11)
             #fig.set_size_inches(11, 8.5)
             
@@ -338,12 +346,12 @@ for varname in varnames:
             outfilename  = varname + "_mrd=" + str(med) + "_cmap="+cmap.name  + ".pdf"
             plt.savefig(outfilename, dpi=600)
             
-            #m.show() # show plot in matplotlib viewer
-            print "wrote", outfilename
+            plt.show() # show plot in matplotlib viewer
+            print("wrote", outfilename)
             plt.clf() # clear canvas for next plot
 	    
 
-print "done"
+print("done")
 sys.exit()
 
 
